8de6551fad85521e749292f865a47df318ee06ea
lgcorzo <lgcorzo@gmail.com>
2025-04-27 00:58:15 +0200

    feat: tool query r2r

--- src/fastapi_autogen_team/tool.py
+++ src/fastapi_autogen_team/tool.py
@@ -1,11 +1,17 @@
 # export R2R_API_KEY=...
+import os
 from r2r import R2RClient
-client = R2RClient() 
-
-
 
 
 def search(query: str):
+  
+  user = os.getenv("R2R_USER")  
+  pwd = os.getenv("R2R_PWD")  
+  base_url = os.getenv("R2R_URL", "http://localhost:7272")
+  client = R2RClient(base_url=base_url) 
+  client.users.login(user, pwd)
 
-
-  return {"query": query}
+  response = client.retrieval.rag(
+    query=query,
+  )
+  return response.to_json()
