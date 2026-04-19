0cc60cc371c1bd3c62e380bc880be161ac95b427
lgcorzo <lgcorzo@gmail.com>
2026-04-19 10:28:10 +0000

    Deploying to gh-pages from @ lgcorzo/fastapi-autogen-team@cea0a19708c46f0d08faed7957e52a69c31598f0 🚀

--- trait.impl/core/error/trait.Error.js
+++ trait.impl/core/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["fastapi_autogen_team",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"fastapi_autogen_team/infrastructure/tools/search/enum.SearchError.html\" title=\"enum fastapi_autogen_team::infrastructure::tools::search::SearchError\">SearchError</a>",0]]]]);
+    const implementors = Object.fromEntries([["fastapi_autogen_team",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"fastapi_autogen_team/infrastructure/tools/search/enum.SearchError.html\" title=\"enum fastapi_autogen_team::infrastructure::tools::search::SearchError\">SearchError</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
